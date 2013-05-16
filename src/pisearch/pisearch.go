21bf0c5799be9cefc124ec532eb138d86ccc8578
Dave Andersen <dga@cs.cmu.edu>
2013-05-15 22:34:36 -0400

    documentation typo fix:  max -> min

--- src/pisearch/pisearch.go
+++ src/pisearch/pisearch.go
@@ -117,7 +117,7 @@ func (pisearch *Pisearch) digitAt(pos int) byte {
 }
 
 // GetDigits returns an ASCII string representation of the digits of
-// pi from position start to max(start+length, end of pi file).
+// pi from position start to min(start+length, end of pi file).
 func (pisearch *Pisearch) GetDigits(start int, length int) (digits string) {
 	end := start + length
 	if start >= pisearch.numDigits {
