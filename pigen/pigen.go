a45d2e40b76238f39869563beb6b80c3e7e74bbf
Dave Andersen <dga@cs.cmu.edu>
2013-06-01 21:29:33 -0400

    Upgraded to the binary splitting version of the algorithm for increased speed.  Why not?  :)

--- pigen/pigen.go
+++ pigen/pigen.go
@@ -1,7 +1,7 @@
 package main
 
 // This is a Go translation of Nick Craig-Wood's Python implementation
-// of fixedpoint Pi computation using the basic Chudnovsky algorithm.
+// of fixedpoint Pi computation using the binary split Chudnovsky algorithm.
 // There are faster ways to compute Pi - grab the GMP Pi demo program
 // if you're serious about it.
 // For a better explanation of the algorithm and cleaner code,
@@ -39,54 +39,62 @@ func chudPi(n int64) *big.Int {
 	C := big.NewInt(640320)
 	C3Div24 := big.NewInt(0).Exp(C, big.NewInt(3), nil)
 	C3Div24.Div(C3Div24, big.NewInt(24))
-	
-	k := big.NewInt(1)
-	a_k := big.NewInt(0).Set(onebase)
-	a_sum := big.NewInt(0).Set(onebase)
-	b_sum := big.NewInt(0)
-	tmp := big.NewInt(0)
+	zero, one, three, five, six := big.NewInt(0), big.NewInt(1), big.NewInt(3), big.NewInt(5), big.NewInt(6)
 
-	zero, one, five, six := big.NewInt(0), big.NewInt(1), big.NewInt(5), big.NewInt(6)
-	sixk := big.NewInt(0)
-	t1, t2, t3, kCubed := &big.Int{}, &big.Int{}, &big.Int{}, &big.Int{}
+	var bs func(a, b *big.Int) (*big.Int, *big.Int, *big.Int)
 
-	for {
-		// -(6*k-5)*(2*k-1)*(6*k-1)
-		sixk.Mul(k, six)
-		t1.Sub(sixk, five)
-		t2.Mul(k, big.NewInt(2)) // Lsh
-		t2.Sub(t2, one)
-		t3.Sub(sixk, one)
-		t1.Mul(t1, t2)
-		t1.Mul(t1, t3)
-		a_k.Mul(a_k, t1.Neg(t1))
-		kCubed.Exp(k, big.NewInt(3), nil)
-		// division: preserve precision
-		a_k.Div(a_k, kCubed.Mul(kCubed, C3Div24))
+	bs = func(a, b *big.Int) (*big.Int, *big.Int, *big.Int) {
+		//fmt.Println("bs ", a, b)
+		Pab, Qab, Tab := big.NewInt(1), big.NewInt(1), big.NewInt(0)
+		bminusa := big.NewInt(0).Sub(b, a)
+		if (one.Cmp(bminusa) == 0) {
+			if (zero.Cmp(a) != 0) {
+				sixa := big.NewInt(0).Mul(a, six)
+				Pab.Sub(sixa, one)
+				sixa.Sub(sixa, five)
+				Pab.Mul(Pab, sixa) // *= 6*a-5
+				sixa.Lsh(a, 1)
+				sixa.Sub(sixa, one)
+				Pab.Mul(Pab, sixa)
 
-		a_sum.Add(a_sum, a_k)
-		b_sum.Add(b_sum, tmp.Mul(a_k, k))
-		
-		k.Add(k, one)
-		if a_k.Cmp(zero) == 0 { // translate
-			break
+				Qab.Exp(a, three, nil)
+				Qab.Mul(Qab, C3Div24)
+			}
+			Tab.SetInt64(545140134)
+			Tab.Mul(Tab, a)
+			Tab.Add(Tab, big.NewInt(13591409))
+			Tab.Mul(Tab, Pab)
+			if a.Bit(0) == 1 {
+				Tab.Neg(Tab)
+			}
+		} else {
+			// m is the midpoint between a & b
+			m := big.NewInt(0).Add(a, b)
+			m.Rsh(m, 1)
+			Pam, Qam, Tam := bs(a, m)
+			Pmb, Qmb, Tmb := bs(m, b)
+			Pab.Mul(Pam, Pmb)
+			Qab.Mul(Qam, Qmb)
+
+			// Tab = Qmb * Tam + Pam * Tmb
+			Qmb.Mul(Qmb, Tam)
+			Pam.Mul(Pam, Tmb)
+			Tab.Add(Qmb, Pam)
 		}
+		return Pab, Qab, Tab
 	}
-	total := big.NewInt(13591409)
-	tmp.SetInt64(545140134)
-	total.Mul(total, a_sum)
-	tmp.Mul(tmp, b_sum)
-	total.Add(total, tmp)
+	DigitsPerTerm := int64(14)
+	startB := big.NewInt(n/DigitsPerTerm + 1)
 
-	tmp.SetInt64(10005)
+	_, q, t := bs(big.NewInt(0), startB)
+	
+	tmp := big.NewInt(10005)
 	tmp.Mul(tmp, onebase)
 	tmp.Mul(tmp, onebase) // tmp = 10005*onebase^2
 	// so sqrt(tmp) = sqrt(10005)*onebase with precision maintained
 	tmp = mathutil.SqrtBig(tmp)
 	tmp.Mul(tmp, big.NewInt(426880))
-	//fmt.Println(tmp)
-	//fmt.Println(total)
-	tmp.Mul(tmp, onebase)
-	tmp.Div(tmp, total)
+	tmp.Mul(tmp, q)
+	tmp.Div(tmp, t)
 	return tmp
 }
